e6d848c5105de9f99211662c861cb13c86f0d3da
Nursultan <nabakirov@GMAIL.COM>
2018-12-21 14:02:39 +0600

    run.bat

--- run.bat
+++ run.bat
@@ -1,3 +1,4 @@
 #! /bin/bash
 
-.env\Scripts\activate && python run_exam_server.py && start "" http://localhost:5000
+.env\Scripts\activate && python run_exam_server.py 
+start "" http://localhost:5000
